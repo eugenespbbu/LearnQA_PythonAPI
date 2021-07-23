3fed88bd7e7d1a45d227026c59716abc76d90e85
eugenespbbu <86985993+eugenespbbu@users.noreply.github.com>
2021-07-24 01:06:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 import requests
+import json
+import time
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.request("get","https://playground.learnqa.ru/ajax/api/longtime_job")
+
+
+string_as_json_format = response.text
+json_text = json.loads(string_as_json_format)
+token1 = json_text['token']
+time1 = json_text['seconds']
+
+
+response = requests.request("get","https://playground.learnqa.ru/ajax/api/longtime_job", params={"token":token1})
+string_as_json_format = response.text
+json_text = json.loads(string_as_json_format)
+
+if json_text['status'] == 'Job is NOT ready':
+    print('Полёт стабильный')
+else:
+    print('Щеф, всё пропало!!111')
+
+
+time.sleep(time1+1)
+response = requests.request("get","https://playground.learnqa.ru/ajax/api/longtime_job", params={"token":token1})
+string_as_json_format = response.text
+json_text = json.loads(string_as_json_format)
+
+if json_text['status'] == 'Job is ready':
+    print('Всё ровно, результат =', json_text['result'])
+else:
+    print('Шоб я еще сел за баранку этого пылесоса!!!')
